0ec70fcdadb41c106995c966a76a746622a40d25
Diya-Srikanth <72697147+Diya-Srikanth@users.noreply.github.com>
2020-12-05 02:00:14 +0100

    Add files via upload

---
+++ lexf.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+if __name__ == '__main__':
+    a = input()
+    l = []
+    c = []
+    n = []
+    final = []
+    b = int(input())
+    listed = a.split()
+    f = list(permutations(listed, b))
+    for x in f:
+        string = list(x)
+        c.append(string)
+
+    for i in range(len(listed)):
+        sim = [listed[i]] * b
+        n.append(sim)
+    data = n + c
+    final = sorted(data)
+    for x in final:
+        print("".join(y for y in x))
